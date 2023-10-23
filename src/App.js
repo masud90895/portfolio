2e8f22338436361faa587edf6f63d634d7380d72
masud90895 <masudhossainmbs129@gmail.com>
2023-10-23 16:34:02 +0600

    data chnages

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Home from './Components/Home';
 import ProjectDitailes from './Components/ProjectDitailes';
 import Main from './Layout/Main';
 
-console.log("object");
-
 
 function App() {
   const router = createBrowserRouter([
